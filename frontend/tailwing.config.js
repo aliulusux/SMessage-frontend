90bfa3c7598d86229f21da9c5098b8899d336fa4
Ali Ulusu <88875819+aliulusux@users.noreply.github.com>
2025-10-15 07:34:01 +0300

    tailwing.config.js

--- frontend/tailwing.config.js
+++ frontend/tailwing.config.js
@@ -1,11 +1,9 @@
-@type {import('tailwindcss').Config}
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: ["./src/*.{js,jsx,ts,tsx}", "./index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
-
 
